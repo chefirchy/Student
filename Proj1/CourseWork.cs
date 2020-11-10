2d1d36f3861280923d77426baf56d307d43c1a9e
chefirchy <42928834+chefirchy@users.noreply.github.com>
2020-11-10 21:54:34 +0200

    Add files via upload

---
+++ Proj1/CourseWork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proj1
+{
+    public class CourseWork
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public CourseWork()
+        {
+            Title = "";
+            Description = "";
+        }
+        public CourseWork(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
+        public override string ToString()
+        {
+            return Title + "$" + Description;
+        }
+    }
+}
---
+++ Proj1/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proj1
+
+{
+    public abstract class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public AddressField Address { get; set; }
+        protected Person()
+        {
+            Name = "xxx";
+            Age = 0;
+            Address = new AddressField();
+        }
+        protected Person(string name, int age, AddressField address)
+        {
+            Name = name;
+            Age = age;
+            Address = address;
+        }
+        public override string ToString()
+        {
+            return Name + "|" + Age + "|" + Address.ToString();
+        }
+        public static bool operator ==(Person a, Person b)
+        {
+            return a.Name == b.Name && a.Age == b.Age && a.Address == b.Address;
+        }
+        public static bool operator !=(Person a, Person b)
+        {
+            return a.Name != b.Name || a.Age != b.Age || a.Address != b.Address;
+        }
+    }
+}
